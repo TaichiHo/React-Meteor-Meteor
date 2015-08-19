f65af56f0b0a37bb978cfdf3dcafc2780ca0c119
Taichi <heyuehengtaichi@gmail.com>
2015-08-18 23:22:51 -0700

    stick with the toolbar

--- client/components/TitleBar.jsx
+++ client/components/TitleBar.jsx
@@ -1,5 +1,5 @@
 const {Navigation, Router} = ReactRouter;
-const {FlatButton, AppBar}  = mui;
+const {FlatButton, Avatar, Toolbar, ToolbarGroup, ToolbarTitle, IconButton, FontIcon, DropDownMenu, AppBar}  = mui;
 const ThemeManager = new mui.Styles.ThemeManager();
 
 
@@ -35,23 +35,51 @@ TitleBar = React.createClass({
             label = this.data.currentUser.emails[0];
         }
 
+        var itemOnRight = <FlatButton label={label} onClick={this.handleClick}/>;
+        if (this.data.currentUser) {
+            itemOnRight =
+                <ToolbarGroup key={1} float="right">
+                    <IconButton touch={true}>
+                        <Avatar size={30}>
+                            A
+                        </Avatar>
+                    </IconButton>
+                </ToolbarGroup>;
+        }
+
+
         return (
-            <AppBar
-                title="Edwo"
-                iconElementRight={
-                    //<SignInButton />
-                    <FlatButton label={label} onClick={this.handleClick} />
-                    }/>
+            <div>
+                <Toolbar title="Edward">
+                    <ToolbarGroup key={0} float="left">
+
+
+                        <ToolbarTitle text="Edwo"/>
+                    </ToolbarGroup>
+                    <ToolbarGroup key={2} float="left">
+                        <IconButton iconClassName="material-icons md-light" tooltipPosition="bottom-center"
+                                    tooltip="Sky">menu</IconButton>
+                    </ToolbarGroup>
+                    {itemOnRight}
+                </Toolbar>
+            </div>
         )
     }
 });
-
-var mySubmitFunc = function(error, state){
+//<ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+//    htmlFor="demo-menu-lower-right">
+//    <li className="mdl-menu__item">Some Action</li>
+//    <li className="mdl-menu__item">Another Action</li>
+//    <li disabled className="mdl-menu__item">Disabled Action</li>
+//    <li className="mdl-menu__item">Yet Another Action</li>
+//</ul>
+var mySubmitFunc = function (error, state) {
 
     if (!error) {
         if (state === "signIn") {
             // Successfully logged in
             // ...
+            console.log(router);
             router.transitionTo('/');
         }
         if (state === "signUp") {
